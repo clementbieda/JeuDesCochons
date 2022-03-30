1c6e0b0f2d7b600e5997259a1b7d4fba1365ec9d
Clément Bieda <98564054+clementbieda@users.noreply.github.com>
2022-03-30 12:03:54 +0200

    Rename script DésView par DiceView

--- Assets/Scripts/DiceView.cs
+++ Assets/Scripts/DiceView.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DésView : MonoBehaviour
+public class DiceView : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
