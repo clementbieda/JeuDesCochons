7d47fe7acde92361ddd1bd78bfd32fb4883edc4f
clementbieda <zorkipad@gmail.com>
2022-04-01 18:41:37 +0200

    Update GameModel.cs

--- Assets/Scripts/GameModel.cs
+++ Assets/Scripts/GameModel.cs
@@ -19,20 +19,39 @@ public class GameModel
 
     public GameModel()
     {
-        for (int i = 0; i < 6; i++)
-        {
-            _faces[i] = i + 1;
-        }
-
         
     }
 
+    public void Tirage()
+    {
+        if (_joueur1)
+        {
+            //On tire 3 dés pour le premier joueur
+            _premierDé = rand.Next(1, 7);
+            _secondDé = rand.Next(1, 7);
+            _troisiemeDé = rand.Next(1, 7);
+        }
+        else
+        {
+            //On tire 3 dés pour le second joueur
+            _premierDé = rand.Next(1, 7);
+            _secondDé = rand.Next(1, 7);
+            _troisiemeDé = rand.Next(1, 7);
+        }
+        //C'est le principe du tour par tour, on change de joueur 
+        
+    }
+
+
     public void VerifyPaire()
     {
+        //On vérifie sur c'est le joueur 1 ou le joueur 2 qui joue
         if (_joueur1)
         {
             if (_premierDé == _secondDé || _premierDé == _troisiemeDé)
             {
+
+                //On ajoute la valeur du dé au score
                 _scoreJ1 += _premierDé;
             }
             else if (_secondDé == _troisiemeDé)
@@ -79,23 +98,17 @@ public class GameModel
 
     public void VerifyCochon()
     {
-
+        if ((_premierDé != _secondDé && _premierDé != _troisiemeDé && _secondDé != _troisiemeDé)
+         && (_premierDé == 1 || _premierDé == 3 || _premierDé == 6)
+         && (_secondDé == 1 || _secondDé == 3 || _secondDé == 6)
+         && (_troisiemeDé == 1 || _troisiemeDé == 3 || _troisiemeDé == 6))
+        {
+            _scoreJ1 += 20;
+        }
     }
 
-    public void Tirage()
+    public void SwitchPlayer()
     {
-        if (_joueur1)
-        {
-            _premierDé = rand.Next(1, 7);
-            _secondDé = rand.Next(1, 7);
-            _troisiemeDé = rand.Next(1, 7);
-        }
-        else
-        {
-            _premierDé = rand.Next(1, 7);
-            _secondDé = rand.Next(1, 7);
-            _troisiemeDé = rand.Next(1, 7);
-        }
         _joueur1 = !_joueur1;
     }
 
